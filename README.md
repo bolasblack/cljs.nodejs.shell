34638e043dad237db2084c9f955409fd51250222
c4605 <bolasblack@gmail.com>
2017-03-04 16:57:04 +0800

    update :in-enc :out-enc behavior describe in document

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 [![cljs.nodejs.shell](https://img.shields.io/clojars/v/org.clojars.c4605/cljs.nodejs.shell.svg)](https://clojars.org/org.clojars.c4605/cljs.nodejs.shell)
 
-Read [https://clojuredocs.org/clojure.java.shell](https://clojuredocs.org/clojure.java.shell) for usage.
+Read [https://clojuredocs.org/clojure.java.shell](https://clojuredocs.org/clojure.java.shell) and [doc-string](https://github.com/bolasblack/cljs.nodejs.shell/blob/master/src/cljs/nodejs/shell.cljs#L13) for usage.
 
 ## Difference between cljs.nodejs.shell, [clojure.java.shell](https://clojuredocs.org/clojure.java.shell), [planck.shell](http://planck-repl.org/planck-namespaces.html)
 
  | cljs.nodejs.shell | clojure.java.shell | planck.shell
 -----|-----|-----|-----
 `:in` in option | `String` or `js/Buffer` | [any legal input source for clojure.java.io/copy](https://github.com/clojure/clojure/blob/fe0cfc71e6ec7b546066188c555b01dae0e368e8/src/clj/clojure/java/shell.clj#L84) | `String` or [file url scheme](https://github.com/mfikes/planck/blob/f16c065ca09e24c9d73191805c402985137e83a9/planck-cljs/src/planck/shell.cljs#L57)
-`:out` in result | `js/Buffer` | `byte[]` or `String` | `String`
+`:out` in result | `String` or `js/Buffer` | `byte[]` or `String` | `String`
 spawn command asynchronously | no | no | [`sh-async`](https://github.com/mfikes/planck/blob/f16c065ca09e24c9d73191805c402985137e83a9/planck-cljs/src/planck/shell.cljs#L80)
 
 ## Leiningen/Boot
--- src/cljs/nodejs/shell.cljs
+++ src/cljs/nodejs/shell.cljs
@@ -14,25 +14,25 @@
   sub-process.
 
   Options are
-  :in      may be given followed by any legal input source for
-           Buffer or String, to be fed to the sub-process's stdin.
-  :in-enc  option may be given followed by a String, used as a character
-           encoding name (for example \"UTF-8\" or \"ISO-8859-1\") to
-           convert the input string specified by the :in option to the
-           sub-process's stdin.  Defaults to UTF-8.
-           If the :in option provides a byte array, then the bytes are passed
-           unencoded, and this option is ignored.
+  :in      js/Buffer or String, to be fed to the sub-process's stdin.
+  :in-enc  option may be given followed by a String. If a String is
+           given, it will be used as a character encoding name (for example
+           \"UTF-8\" or \"ISO-8859-1\") to convert the input string
+           specified by the :in option to the sub-process's stdin.
+           Defaults to UTF-8 if :in is String, or do nothing if :in is
+           js/Buffer.
   :out-enc option may be given followed by a String. If a String is
            given, it will be used as a character encoding name (for example
            \"UTF-8\" or \"ISO-8859-1\") to convert the sub-process's stdout
-           to a String which is returned. Defaults to UTF-8.
+           to a String which is returned.
+           Defaults do nothing and return js/Buffer.
   :env     override the process env with a map.
   :dir     override the process dir with a String.
   You can bind :env or :dir for multiple operations using with-sh-env
   and with-sh-dir.
   sh returns a map of
     :exit => sub-process's exit code
-    :out  => sub-process's stdout (as Buffer or String)
+    :out  => sub-process's stdout (as js/Buffer or String)
     :err  => sub-process's stderr (String via platform default encoding)"
   [& args]
   (let [[cmd {:keys [in in-enc out-enc env dir]}] (split-with string? args)
