880c861e4e513cdcf63210bd4cf9750ceae809cd
c4605 <bolasblack@gmail.com>
2018-07-01 20:48:22 +0800

    add cljs.spec

--- src/cljs/nodejs/shell.cljs
+++ src/cljs/nodejs/shell.cljs
@@ -1,28 +1,29 @@
 (ns cljs.nodejs.shell
   (:require-macros [cljs.nodejs.shell])
-  (:require ["child_process" :as child-process]
+  (:require [cljs.spec.alpha :as s]
+            ["child_process" :as child-process]
             ["iconv-lite" :as iconv]))
 
 (def ^:dynamic *sh-dir* nil)
 (def ^:dynamic *sh-env* nil)
 
-(defn- deserialize-opts [args]
-  (let [[cmd rest-args] (split-with string? args)
-        [origin-options cbs] (split-with (complement fn?) rest-args)
-        {:keys [in in-enc out-enc env dir]} origin-options
+(defn- deserialize-opts [cmd opts]
+  (let [{:keys [in in-enc out-enc env dir]} opts
         decoded-in (if (nil? in-enc) in (iconv/decode in in-enc))
-        spawn-opts {:input decoded-in :env (or env *sh-env*) :cwd (or dir *sh-dir*)}]
+        spawn-opts {:input decoded-in
+                    :env (or env *sh-env*)
+                    :cwd (or dir *sh-dir*)}]
+    (when-not (s/valid? (s/nilable ::string-string-map?) (:env spawn-opts))
+      (throw (js/Error. (s/explain-str ::string-string-map? (:env spawn-opts)))))
     {:bin (first cmd)
      :args (or (next cmd) '())
-     :options spawn-opts
-     :origin-options origin-options
-     :cb (first cbs)}))
+     :options spawn-opts}))
 
 (defn- deserialize-spawn-result
-  [{exit :exit signal :signal error :error raw-out :stdout raw-err :stderr}
+  [{:keys [exit signal error stdout stderr] :as args}
    {:keys [out-enc]}]
-  (let [decoded-out (if (and raw-out out-enc) (iconv/decode raw-out out-enc) raw-out)
-        decoded-err (if (and raw-err out-enc) (iconv/decode raw-err out-enc) raw-err)]
+  (let [decoded-out (if (and stdout out-enc) (iconv/decode stdout out-enc) stdout)
+        decoded-err (if (and stderr out-enc) (iconv/decode stderr out-enc) stderr)]
     {:exit exit
      :signal signal
      :error error
@@ -60,9 +61,10 @@
     :signal => The signal used to kill the child process (String)
     :error  => The error object if the child process failed or timed out (Error)
     :out  => sub-process's stdout (as js/Buffer or String)
-    :err  => sub-process's stderr (String via platform default encoding)"
+    :err  => sub-process's stderr (as js/Buffer or String)"
   [& args]
-  (let [{:keys [bin args options origin-options]} (deserialize-opts args)
+  (let [{:keys [cmd opts]} (s/conform ::sh-args args)
+        {:keys [bin args options]} (deserialize-opts cmd opts)
         spawn-result (child-process/spawnSync bin (clj->js args) (clj->js options))]
     (deserialize-spawn-result
      {:exit spawn-result.status
@@ -70,7 +72,7 @@
       :error spawn-result.error
       :stdout spawn-result.stdout
       :stderr spawn-result.stderr}
-     origin-options)))
+     opts)))
 
 (defn sh-async
   "Passes the given strings to child_process.spawn() to launch a
@@ -104,9 +106,10 @@
     :signal => The signal used to kill the child process (String)
     :error  => The error object if the child process failed or timed out (Error)
     :out    => sub-process's stdout (as js/Buffer or String)
-    :err    => sub-process's stderr (String via platform default encoding)"
+    :err    => sub-process's stderr (as js/Buffer or String)"
   [& args]
-  (let [{:keys [bin args options origin-options cb]} (deserialize-opts args)
+  (let [{:keys [cmd opts cb]} (s/conform ::sh-async-args args)
+        {:keys [bin args options]} (deserialize-opts cmd opts)
         process (child-process/spawn bin (clj->js args) (clj->js options))
         empty-buf (js/Buffer.alloc 0)
         spawn-results (volatile! {:finished? false
@@ -141,7 +144,7 @@
                :error err
                :stdout (:stdout @spawn-results)
                :stderr (:stderr @spawn-results)}
-              origin-options)))))
+              opts)))))
 
     (.once
      process
@@ -157,4 +160,47 @@
                :error nil
                :stdout (:stdout @spawn-results)
                :stderr (:stderr @spawn-results)}
-              origin-options)))))))
+              opts)))))))
+
+
+
+(s/def ::string-string-map?
+  (s/and map? (s/every-kv string? string?)))
+
+(s/def ::js-buffer?
+  (s/with-gen #(instance? js/Buffer %)
+    #(s/gen #{(js/Buffer.from "Generated Buffer")})))
+
+(s/def ::exit integer?)
+(s/def ::signal string?)
+(s/def ::error (s/with-gen #(instance? js/Error %)
+                 #(s/gen #{(js/Error. "Generated Error")})))
+(s/def ::out (s/or :string string?
+                   :buffer ::js-buffer?))
+(s/def ::err (s/or :string string?
+                   :buffer ::js-buffer?))
+
+(s/def ::sh-result
+  (s/keys :req-un [::exit ::signal ::error ::out ::err]))
+
+(s/def ::sh-opt
+  (s/alt :in (s/cat :key #{:in} :val string?)
+         :in-enc (s/cat :key #{:in-enc} :val string?)
+         :out-enc (s/cat :key #{:out-enc} :val string?)
+         :dir (s/cat :key #{:dir} :val string?)
+         :env (s/cat :key #{:env} :val ::string-string-map?)))
+
+(s/def ::sh-args
+  (s/cat :cmd (s/+ string?)
+         :opts (s/* ::sh-opt)))
+(s/def ::sh-async-args
+  (s/cat :cmd (s/+ string?)
+         :opts (s/* ::sh-opt)
+         :cb fn?))
+
+(s/fdef sh
+  :args ::sh-args
+  :ret ::sh-result)
+(s/fdef sh-async
+  :args ::sh-async-args
+  :ret nil?)
--- test/cljs/nodejs/shell_test.cljs
+++ test/cljs/nodejs/shell_test.cljs
@@ -1,6 +1,9 @@
 (ns cljs.nodejs.shell-test
-  (:require [cljs.test :refer [deftest are is] :refer-macros [async]]
-            [clojure.string :as s]
+  (:require [clojure.test.check]
+            [clojure.test.check.generators]
+            [clojure.test.check.properties]
+            [cljs.test :refer [deftest are is] :refer-macros [async]]
+            [clojure.string :as str]
             [cljs.nodejs.shell :as sh :include-macros true]))
 
 (deftest shell
@@ -41,7 +44,7 @@
       (done)))))
 
 (deftest sh-with-sh-dir-test
-  (is (s/ends-with? (.toString (:out (sh/with-sh-dir "/tmp" (sh/sh "pwd")))) "/tmp\n"))
+  (is (str/ends-with? (.toString (:out (sh/with-sh-dir "/tmp" (sh/sh "pwd")))) "/tmp\n"))
   (let [res (sh/with-sh-dir "bogus" (sh/sh "pwd"))]
     (is (not= (:exit res) 0))
     (is (= "spawnSync pwd ENOENT" (.-message (:error res))))))
@@ -53,7 +56,7 @@
      (sh/sh-async
       "pwd"
       (fn [res]
-        (is (s/ends-with? (.toString (:out res)) "/tmp\n"))
+        (is (str/ends-with? (.toString (:out res)) "/tmp\n"))
         (done))))))
 (deftest sh-async-with-sh-dir-test-non-exist-bin
   (async
@@ -68,11 +71,21 @@
         (done))))))
 
 (deftest sh-with-sh-env-test
+  (is (thrown-with-msg? js/Error #"cljs\.spec\.alpha"
+                        (sh/with-sh-env {"FOO" {}}
+                          (sh/sh "env"))))
   (is (= "FOO=BAR\n" (.toString (:out (sh/with-sh-env {"FOO" "BAR"}
                                         (sh/sh "env")))))))
 (deftest sh-async-with-sh-env-test
   (async
    done
+   (is (thrown-with-msg? js/Error #"cljs\.spec\.alpha"
+                         (sh/with-sh-env {"FOO" {}}
+                           (sh/sh-async
+                            "env"
+                            (fn [res]
+                              (is (= "FOO=BAR\n" (.toString (:out res))))
+                              (done))))))
    (sh/with-sh-env {"FOO" "BAR"}
      (sh/sh-async
       "env"
